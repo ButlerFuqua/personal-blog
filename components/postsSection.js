f2b5a1ce316d1b0a30ff02c9b4b9ec6472df3df3
Butler Fuqua <butlerfuqua@gmail.com>
2020-07-24 08:48:58 -0400

    Remove utilStyles from postsSection component.

--- components/postsSection.js
+++ components/postsSection.js
@@ -1,18 +1,19 @@
-import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import Date from '../components/date'
+
 
 export default function PostsSection({ posts, title }) {
     return (
         <>
-            <h2 className={utilStyles.headingLg}>{title}</h2>
+            <h2 >{title}</h2>
             <ul >
                 {posts.map(({ id, date, title }) => (
-                    <li className={utilStyles.listItem} key={id}>
+                    <li key={id}>
                         <Link href="/posts/[id]" as={`/posts/${id}`}>
                             <a>{title}</a>
                         </Link>
                         <br />
-                        <small className={utilStyles.lightText}>
+                        <small >
                             <Date dateString={date} />
                         </small>
                     </li>
