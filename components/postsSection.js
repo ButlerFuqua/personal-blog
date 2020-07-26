6477137d452a89e5546bb7d22cca36f519f9264b
Butler Fuqua <butlerfuqua@gmail.com>
2020-07-26 10:58:07 -0400

    Add extra content div on postsSection

--- components/postsSection.js
+++ components/postsSection.js
@@ -6,16 +6,36 @@ const PostsList = styled.ul`
     padding: 0;
     margin: 0;
 
-    & li {
-        list-style: none;
-        margin: 1rem 0;
-        border-radius: 5px;
+`
 
-        transition: .3s;
-        transform: scale(1.0)
+const Li = styled.li`
 
+    list-style: none;
+    margin: 1rem 0;
+    border-radius: 5px;
+    padding: 1rem;
+
+    transition: .4s;
+    transform: scale(1.0);
+
+
+    & .extra {
+        font-size: 1rem;
+        max-height: 0;
+        overflow: hidden;
+        transition: .5s;
     }
 
+    &:hover{
+       @media(min-width: 769px){
+        transform: scale(1.02);
+        box-shadow: 0px 0px 20px 6px rgba(0,0,0, .1);
+
+        & .extra {
+            max-height: 900px;
+        }
+       }
+    }
 `
 
 export default function PostsSection({ posts, title, type }) {
@@ -24,15 +44,18 @@ export default function PostsSection({ posts, title, type }) {
             <h2 >{title}</h2>
             <PostsList >
                 {posts.map(({ id, date, title }) => (
-                    <li key={id}>
+                    <Li key={id}>
                         <Link href={`/${type}/[id]`} as={`/${type}/${id}`}>
                             <a>{title}</a>
                         </Link>
+                        <div className="extra">
+                            <p>More information will go here</p>
+                        </div>
                         <br />
                         <small >
                             <Date dateString={date} />
                         </small>
-                    </li>
+                    </Li>
                 ))}
             </PostsList>
         </>
