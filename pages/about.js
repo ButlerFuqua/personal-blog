9c082a81121f87fd87ae39a2e8f68832d97647d7
Butler Fuqua <butlerfuqua@gmail.com>
2020-07-29 06:12:29 -0400

    Add test styled components to about page

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,21 @@
 import Layout from '../components/layout'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+    font-size: 3rem;
+    color: blue;
+`
+const P = styled.p`
+    color: orange:
+    font-size: 1.2rem;
+    margin: 0;
+`
 
 export default function About() {
     return (
         <Layout home>
-            <h1>About</h1>
+            <Title>About</Title>
+            <P>This is a test</P>
         </Layout>
     )
 }
