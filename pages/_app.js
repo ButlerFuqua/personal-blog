c97932074a4675d8f560f768d5c4a97794e29645
Butler Fuqua <butlerfuqua@gmail.com>
2020-07-29 22:26:46 -0400

    Bring over _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -9,13 +9,57 @@ const theme = {
   },
 }
 
+
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
     return (
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
+
+        <style jsx global>{`
+    html,
+    body {
+      padding: 0;
+      margin: 0;
+      font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+        Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      line-height: 1.6;
+      font-size: 18px;
+      background: #F4F4F4;
+    }
+
+    * {
+      box-sizing: border-box;
+    }
+
+    a {
+      color: #0070f3;
+      text-decoration: none;
+    }
+
+    a:hover {
+      text-decoration: underline;
+    }
+
+    img {
+      max-width: 100%;
+      display: block;
+    }
+
+    main {
+      display: flex;
+    }
+
+    article{
+      background: white;
+      padding: 1rem;
+    }
+    `}</style>
+
       </ThemeProvider>
+
     )
   }
 }
+
